b04f5c75e1c67b2110886aed11c77f48209c0bc3
LaTanya Donaldson and Zach Bastian <latanya@>
2017-03-22 16:37:51 -0700

    it will add more functions.

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,10 @@ $(document).ready(function(){
     var nameInput = $("input#name").val();
     var addressInput = $("input#address").val();
     var numberInput = $("input#number").val();
+
+    $(".name").text(nameInput);
+    $(".address").text(addressInput);
+    $(".number").text(numberInput);
+
   });
 });
